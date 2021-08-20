0ccb245d7b0a8e1fea7b8a39610001e6516a516d
Shahmar <78316758+shahmar10@users.noreply.github.com>
2021-08-20 12:10:13 +0400

    Done Task

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -9,5 +9,37 @@ class Country extends Model
 {
     use HasFactory;
 
+    protected $appends = ['result_count'];
+
+    public function results(){
+
+        return $this->belongsToMany(Sport::class,'results','country_id','sport_id')->withPivot('result');
+
+    }
+
+    public function getResultCountAttribute(){
+
+        $gold   = 0;
+        $silver = 0;
+        $bronze = 0;
+
+        foreach ($this->results as $result) {
+            switch ($result->pivot->result) {
+            case 1:
+                $gold++;
+                break;
+            case 2:
+                $silver++;
+                break;
+            case 3:
+                $bronze++;
+                break;
+            }
+        }
+
+        return ['gold' => $gold, 'silver' => $silver, 'bronze' => $bronze];
+
+    }
+
     protected $fillable = ['name', 'short_code'];
 }
---
+++ app/Models/Result.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Result extends Model
+{
+    use HasFactory;
+
+
+}
