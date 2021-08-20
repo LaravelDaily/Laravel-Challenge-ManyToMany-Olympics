c642971ae69ce2dcbccd86241619abe9abdbaf9a
wahdam <wahdam.amarulloh@gmail.com>
2021-08-20 20:06:53 +0700

    final

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -10,4 +10,37 @@ class Country extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'short_code'];
+
+    public function getId(String $shorCode)
+    {
+        return $this->select('id')->where('short_code', $shorCode)->first()->id ?? 0;
+    }
+
+    /**
+     * Get all of the medals for the Country
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function getGold()
+    {
+        return $this->hasMany(CountryMedal::class, 'gold', 'id');
+    }
+    /**
+     * Get all of the silver medals for the Country
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function getSilver()
+    {
+        return $this->hasMany(CountryMedal::class, 'silver', 'id');
+    }
+    /**
+     * Get all of the bronze medals for the Country
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function getBronze()
+    {
+        return $this->hasMany(CountryMedal::class, 'bronze', 'id');
+    }
 }
