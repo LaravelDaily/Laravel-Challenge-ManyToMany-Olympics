44a2aaf6ac8c31b9421a5e66b9e4de92eb1ad6db
hayaIshida <49195006+hayaIshida@users.noreply.github.com>
2021-08-20 14:26:18 +0200

    fix duplicate ids for selects

--- resources/views/sports/create.blade.php
+++ resources/views/sports/create.blade.php
@@ -16,7 +16,7 @@
                                     <label for="first" class="col-md-4 col-form-label text-md-right">1st place:</label>
 
                                     <div class="col-md-6">
-                                        <select name="first" id="first"
+                                        <select name="first" 
                                                 class="form-control @error('first') is-invalid @enderror">
                                             <option>-- choose country --</option>
                                             @foreach ($countries as $country)
@@ -35,7 +35,7 @@
                                     <label for="second" class="col-md-4 col-form-label text-md-right">2nd place:</label>
 
                                     <div class="col-md-6">
-                                        <select name="second" id="second"
+                                        <select name="second" 
                                                 class="form-control @error('second') is-invalid @enderror">
                                             <option>-- choose country --</option>
                                             @foreach ($countries as $country)
@@ -54,7 +54,7 @@
                                     <label for="third" class="col-md-4 col-form-label text-md-right">3rd place:</label>
 
                                     <div class="col-md-6">
-                                        <select name="third" id="third"
+                                        <select name="third"
                                                 class="form-control @error('third') is-invalid @enderror">
                                             <option>-- choose country --</option>
                                             @foreach ($countries as $country)
@@ -75,7 +75,7 @@
 
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
+                            <button type="submit" class="btn btn-primary" id="sport_submit">
                                 Submit
                             </button>
                         </div>
@@ -86,3 +86,4 @@
         </div>
     </div>
 @endsection
+
