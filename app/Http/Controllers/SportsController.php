2192f8137a21c82e98f91233974219a8e21fb9df
michaelnabil23 <46572405+michaelnabil23@users.noreply.github.com>
2021-08-22 22:49:50 +0200

    I added the cache to the query `top_countries`

--- app/Http/Controllers/SportsController.php
+++ app/Http/Controllers/SportsController.php
@@ -11,9 +11,13 @@ class SportsController extends Controller
 {
     public function create()
     {
-        $sports = Sport::all();
-        $countries = Country::all();
         $places = get_places();
+        $countries = cache()->rememberForever('countries', function () {
+            return Country::all();
+        });
+        $sports = cache()->rememberForever('sports', function () {
+            return Sport::all();
+        });
         return view('sports.create', compact('sports', 'countries', 'places'));
     }
 
@@ -29,19 +33,22 @@ class SportsController extends Controller
         } catch (\Exception $exception) {
             return redirect()->back()->withErrors($exception->getMessage());
         }
+        cache()->forget('top_countries');
         return redirect()->route('show');
     }
 
     public function show()
     {
         $places = get_places();
-        $countries = Country::query()
-            ->withCountPlace()
-            ->orderByCountPlace()
-            ->havingCountPlace()
-            ->take(5)
-            ->get();
-
+        // Can remove line 36 in code and replace `rememberForever` to `remember` and add time for query if the If there are many places to enter a place into the country
+        $countries = cache()->rememberForever('top_countries', function () {
+            return Country::query()
+                ->withCountPlace()
+                ->orderByCountPlace()
+                ->havingCountPlace()
+                ->take(5)
+                ->get();
+        });
         return view('sports.show', compact('countries', 'places'));
     }
 }
--- resources/views/sports/show.blade.php
+++ resources/views/sports/show.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Top 5 Results</div>
+                    <div class="card-header">Top {{ $countries->count() }} Results</div>
                     <div class="card-body">
                         <table class="table table-striped">
                             <thead>
