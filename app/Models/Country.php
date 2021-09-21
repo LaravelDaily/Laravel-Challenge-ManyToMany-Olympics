3b69a54e4d2c577977cb3cafc9ee38770545363d
Victor Gonzalez Toledo <v.gonzalez@cotronic.es>
2021-09-21 10:49:51 +0200

    CREATE: Models

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -10,4 +10,22 @@ class Country extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'short_code'];
+    public function gold_medals()
+    {
+        return $this->belongsToMany(Sport::class)
+            ->withPivot(['position'])
+            ->where('position', 1);
+    }
+    public function silver_medals()
+    {
+        return $this->belongsToMany(Sport::class)
+            ->withPivot(['position'])
+            ->where('position', 2);
+    }
+    public function bronze_medals()
+    {
+        return $this->belongsToMany(Sport::class)
+            ->withPivot(['position'])
+            ->where('position', 3);
+    }
 }
