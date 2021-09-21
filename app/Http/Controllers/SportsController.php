4998e6e1ad55da2de9e7ad8d6e8b6664a4b12a5e
Victor Gonzalez Toledo <v.gonzalez@cotronic.es>
2021-09-21 10:50:59 +0200

    UPDATE: Sports Controller

--- app/Http/Controllers/SportsController.php
+++ app/Http/Controllers/SportsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Sport;
 use App\Models\Country;
+use App\Http\Requests\SportMedalsStoreRequest;
 use Illuminate\Http\Request;
 
 class SportsController extends Controller
@@ -16,17 +17,46 @@ class SportsController extends Controller
         return view('sports.create', compact('sports', 'countries'));
     }
 
-    public function store(Request $request)
+    public function store(SportMedalsStoreRequest $request)
     {
-        // Add your code here
-
+        $sports = $request->input('sport');
+        $firsts = $request->input('first');
+        $seconds = $request->input('second');
+        $thirds = $request->input('third');
+        
+        foreach ($sports as $index => $sport_id){
+            $sport = Sport::findOrFail($sport_id);
+            $sport->countries()->sync([]);
+            $sport->countries()->attach(
+                Country::where('short_code', $firsts[$index])->firstOrFail()->id,
+                ['position' => 1]
+            );
+            $sport->countries()->attach(
+                Country::where('short_code', $seconds[$index])->firstOrFail()->id,
+                ['position' => 2]
+            );
+            $sport->countries()->attach(
+                Country::where('short_code', $thirds[$index])->firstOrFail()->id,
+                ['position' => 3]
+            );            
+        }
         return redirect()->route('show');
     }
 
     public function show()
     {
-        // Add your code here
-
-        return view('sports.show');
+        $countries = Country::select(['name'])
+            ->withCount('gold_medals')
+            ->withCount('silver_medals')
+            ->withCount('bronze_medals')
+            ->havingRaw('gold_medals_count')
+            ->orHavingRaw('silver_medals_count')
+            ->orHavingRaw('bronze_medals_count')
+            ->orderBy('gold_medals_count', 'desc')
+            ->orderBy('silver_medals_count', 'desc')
+            ->orderBy('bronze_medals_count', 'desc')
+            ->orderBy('name', 'asc')
+            ->get();
+        return view('sports.show', compact('countries'));
     }
 }
