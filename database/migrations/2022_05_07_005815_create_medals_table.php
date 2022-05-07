24ed6bf435ff5b4a62ed619008f22abba5578e67
Thiago R Costa <thiago_8777@hotmail.com>
2022-05-07 20:43:54 -0300

    Store medals and showing rank

---
+++ database/migrations/2022_05_07_005815_create_medals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medals', function (Blueprint $table) {
+            $table->foreignId('sport_id')->constrained();
+            $table->foreignId('country_id')->constrained();
+            $table->unsignedInteger('gold')->default(0);
+            $table->unsignedInteger('silver')->default(0);
+            $table->unsignedInteger('bronze')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medals');
+    }
+}
